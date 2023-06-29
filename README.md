6935f25d0cf963d6fbc9af1ce7b264de69b08ca2
Muhammad Usman <muhammad.usman@bitflex.com>
2023-06-29 15:55:44 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FavOnBoardingKit
+
+A description of this package.
---
+++ Tests/FavOnBoardingKitTests/FavOnBoardingKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FavOnBoardingKit
+
+final class FavOnBoardingKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FavOnBoardingKit().text, "Hello, World!")
+    }
+}
